6809d35f0ce43075e654c5b8e9dbb402f8f1a78d
SamGoody <siteroller@gmail.com>
2013-06-10 13:06:49 +0300

    Added moorte.php, which creates compressed and uncompressed cached versions of the file, before spitting out a noncompressed version (for debugging). Added JShrink to Assets, to enable compression on the fly. NOTE: JShrink has it's own license, included in the header of the file!!!!

---
+++ Source/moorte.php
@@ -0,0 +1,14 @@
+<?php 
+$script = $shrunk = '';
+$files = ['Core', 'Range', 'Utilities', 'Extensions', 'Word10'];
+require __DIR__ . "/Assets/scripts/JShrink.php";
+foreach ($files as $file){
+	$js = file_get_contents("$file.js");
+	$script .= "\n".$js;
+	$shrunk .= "\n".JShrink\Minifier::minify($js);
+	//$script .= "\n".JShrink\Minifier::minify($js, array('flaggedComments' => false));
+	}
+file_put_contents('moorte.js',$script);
+file_put_contents('moorte.compressed.js',$shrunk);
+echo $script;
+?>
